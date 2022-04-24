f80fc1b72753b0a13f0d24c678d9346642f7414d
krishna <75976947+krishna050702@users.noreply.github.com>
2022-04-24 12:38:19 +0530

    24_04_22

---
+++ PRIME.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    bool flag=0;
+    for(int i=2;i<sqrt(n);i++)
+    {
+        if(n%i==0)
+        {
+        cout<<"Non-prime\n";
+        flag=1;
+        break;
+        }
+
+    }
+    if(flag==0)
+    cout<<"Prime\n";
+}
